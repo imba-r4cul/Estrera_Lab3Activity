762a8359a9ba09a4721c8fc7035400ffed62eafb
rico <raculxd@gmail.com>
2026-02-27 21:28:26 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console/console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) =>  {
+    res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) =>  {
+    res.send('About Us');
+});
+
+app.listen(port, () => {
+    console.log('Server running at http://localhost:${port}');
+});
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`received: ${JSON.stringify(data)}`);
+});
